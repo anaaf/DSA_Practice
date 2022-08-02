911068cd47ed0e638d51efe38f7ea4cf894aa895
anaaf <anaaf5555@gmail.com>
2022-08-03 01:12:22 +0500

    Add project files.

---
+++ DSA_Practice/BinaryTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+using DSA_Practice;
+namespace DSA_Practice
+{
+    class BinaryTree
+    {
+        TreeNode node1 = new TreeNode(30);
+
+    }
+}
---
+++ DSA_Practice/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA_Practice
+{
+    class TreeNode
+    {
+
+        public TreeNode left;
+        public TreeNode right;
+        public int val;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
