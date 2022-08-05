a2a9fcfb87e87cb1533922f93d337c46e9190778
anaaf <anaaf5555@gmail.com>
2022-08-06 03:09:14 +0500

    nearest court

---
+++ DSA_Practice/NearestCourt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DSA_Practice
+{
+	public class NearestCourt
+	{
+		public static void Main()
+		{
+			int tests = int.Parse(Console.ReadLine());
+
+			for (int t = 1; t <= tests; t++)
+			{
+
+				var line = Console.ReadLine();
+
+				var input = lineSplitter(line);
+
+				var X = input[0];
+				var Y = input[1];
+
+				var mid = Math.Abs(X-Y) / 2.0;
+
+				Console.WriteLine(Math.Ceiling(mid));
+			}
+		}
+
+		public static List<int> lineSplitter(string line)
+		{
+
+			IEnumerable<string> input = line.Split(" ");
+
+			List<int> finalInput = new List<int>();
+
+			foreach (var item in input)
+			{
+				finalInput.Add(int.Parse(item));
+
+			}
+
+			return finalInput;
+		}
+
+
+	}
+}
